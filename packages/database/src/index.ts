5a6124b1d2b656dae78269df3d123a4781b81946
Raghavendra Prasad <raghucs@gmail.com>
2026-07-14 01:45:00 +0100

    feat(db): remove theory/exam-board models; rewrite barrel exports

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,23 +1,17 @@
 export { connectToDatabase, disconnectFromDatabase } from "./connection";
 
 export { User } from "./models/user";
-export { Module } from "./models/module";
-export { QuestionTemplate } from "./models/question-template";
-export { GeneratedQuestion } from "./models/generated-question";
+export { ProgrammingTopic } from "./models/programming-topic";
+export { Question } from "./models/question";
+export { QuestionProgress } from "./models/question-progress";
 export { QuestionAttempt } from "./models/question-attempt";
 export { HintEvent } from "./models/hint-event";
 export { StudySession } from "./models/study-session";
-export { StudentProgress } from "./models/student-progress";
-export { SpecTopic } from "./models/spec-topic";
 
 export type { IUser } from "./models/user";
-export type { IModule } from "./models/module";
-export type { IQuestionTemplate } from "./models/question-template";
-export type { IGeneratedQuestion, ITestCase } from "./models/generated-question";
-export type { IQuestionAttempt } from "./models/question-attempt";
+export type { IProgrammingTopic } from "./models/programming-topic";
+export type { IQuestion, ITestCase, QuestionDifficulty, QuestionType } from "./models/question";
+export type { IQuestionProgress } from "./models/question-progress";
+export type { IQuestionAttempt, IAttemptTestResult } from "./models/question-attempt";
 export type { IHintEvent } from "./models/hint-event";
 export type { IStudySession } from "./models/study-session";
-export type { IStudentProgress, IModuleProgress, IWeakArea, IModuleWeakAreaFlags } from "./models/student-progress";
-export type { ISpecTopic } from "./models/spec-topic";
-
-export { seedEdexcelSpec, seedEdexcelModules } from "./seeds/edexcel-spec";
